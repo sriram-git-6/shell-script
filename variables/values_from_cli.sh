12660afdd0e1efa648c3954474533fe51bee9e7c
sriram <sriram.mudunuru@gmail.com>
2024-02-11 22:37:42 +0530

    changes

---
+++ variables/values_from_cli.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "hello-world, iam learning shell script"
+
+person1=$1 # first value from cli
+person2=$2 # second value from cli
+
+echo "$person1: Hi $person2, good morning"
+echo "$person2: Hi $person1, good morning"
+echo "$person1: how r u $person2"
+echo "$person2: Iam good $person1"
