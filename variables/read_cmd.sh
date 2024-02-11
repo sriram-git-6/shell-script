aee63577d330bd2268ad99c4da868f371d759486
sriram <sriram.mudunuru@gmail.com>
2024-02-11 23:42:13 +0530

    changes

--- variables/read_cmd.sh
+++ variables/read_cmd.sh
@@ -2,12 +2,12 @@
 
 echo "please enter your username"
 
-read -s USERNAME # -s option used not show input coming from terminal
+read -s USERNAME # read command is used to read the line from input. And -s option used not show input coming from terminal
 
 echo "username entered is: $USERNAME"
 
 echo "please enter your password"
 
-read -s PASSWORD # -s option used not show input coming from terminal
+read -s PASSWORD 
 
 echo "password entered is: $PASSWORD"
