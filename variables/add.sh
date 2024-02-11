0980e7af98d3e49a753702c63e259b72df15c7bb
sriram <sriram.mudunuru@gmail.com>
2024-02-11 22:53:57 +0530

    changes

---
+++ variables/add.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+NUMBER1=$1
+NUMBER2=$2
+
+SUM=$((NUMBER1+NUMBER2))
+
+
+echo "addition of two numbers is: $SUM"
--- variables/values_from_cli.sh
+++ variables/values_from_cli.sh
@@ -9,3 +9,6 @@ echo "$person1: Hi $person2, good morning"
 echo "$person2: Hi $person1, good morning"
 echo "$person1: how r u $person2"
 echo "$person2: Iam good $person1"
+
+# while running the sh file: sh values_from_cli.sh rohit kohli
+
