be13fe83c5fb79c6ebabfa70499a5851ac4ff3c0
sriram <sriram.mudunuru@gmail.com>
2024-02-11 23:32:41 +0530

    changes

--- variables/read_cmd.sh
+++ variables/read_cmd.sh
@@ -5,3 +5,9 @@ echo "please enter your username"
 read USERNAME 
 
 echo "username entered is: $USERNAME"
+
+echo "please enter your password"
+
+read PASSWORD
+
+echo "password entered is: $PASSWORD"
