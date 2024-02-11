3f95acb78d0066cf4bacfa8a0157d40195053c77
sriram <sriram.mudunuru@gmail.com>
2024-02-11 15:49:21 +0530

    first-commit

--- hello-world.sh
+++ hello-world.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "hello-world, iam learning shell script"
+
+echo "Ramesh: Hi Suresh, good morning"
+echo "Suresh: Hi Ramesh, good morning"
+echo "Ramesh: how r u Suresh"
+echo "Suresh: Iam good Ramesh"
