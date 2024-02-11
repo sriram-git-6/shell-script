9a246e5adcadbf87a7c5dff75a6254338ae2f3a7
sriram <sriram.mudunuru@gmail.com>
2024-02-12 00:10:01 +0530

    changes

--- variables/array.sh
+++ variables/array.sh
@@ -3,3 +3,5 @@
 persons=("rohit" "kohli" "sachin") # this is called as an array to hold multiple values we use array.
 
 echo "first person is: ${persons[0]}"
+
+echo "all persons in the array are: ${[@]}"
