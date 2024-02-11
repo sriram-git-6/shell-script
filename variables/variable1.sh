8d8e378460bbfd0099b0536b1f2e3a75b5e9cf4b
sriram <sriram.mudunuru@gmail.com>
2024-02-11 22:21:26 +0530

    changes

---
+++ variables/variable1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "hello-world, iam learning shell script"
+
+person1=Ramesh
+person2=Suresh
+
+echo "$person1: Hi $person2, good morning"
+echo "$person2: Hi $person1, good morning"
+echo "$person1: how r u $person2"
+echo "$person2: Iam good $person1"
