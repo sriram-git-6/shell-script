a0d7bb960ce9c716c86510da34d3ec0ffb229c5a
sriram <sriram.mudunuru@gmail.com>
2024-02-12 17:45:53 +0530

    conditions

---
+++ conditions/if_else.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# check if the number is greater than 10 or not.
+
+NUMBER=$1
+
+if [ $NUMBER -gt 10 ]
+
+then 
+
+    echo "$NUMBER is greater than 10"
+
+else
+
+    echo "$NUMBER is not greater than 10"
+
+fi
