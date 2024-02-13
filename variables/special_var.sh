729ed801b3f16d994ab34ae2dd448c2dc6ab58db
sriram <sriram.mudunuru@gmail.com>
2024-02-13 17:53:12 +0530

    special-variables

--- variables/special_var.sh
+++ variables/special_var.sh
@@ -8,4 +8,7 @@ var4=$4
 echo "variable 1: $var1"
 echo "variable 2: $var2"
 
-echo "all variables are: $@"
+echo "all variables are: $@" # all the positional parameters passed to the script or function.
+
+echo "number of variables passed are: $#" # display the number of arguments passed to a script or function
+
