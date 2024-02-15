c9c53323b50250de3910fc8465a602fe061b290b
sriram <sriram.mudunuru@gmail.com>
2024-02-15 07:25:48 +0530

    delete old files

--- delete_oldlogs.sh
+++ delete_oldlogs.sh
@@ -12,7 +12,7 @@
 
 APP_LOGS_DIR=/home/centos/app-logs # This is the directory u have find and delete all log files which were created 2 weeks back
 
-Date=$(date +%F)
+Date=$(date +%F:%H:%M:%S)
 LOGSDIR=/home/centos/shellscript-logs
 #/home/centos/shellscript-logs/scriptname-date.log
 script_name=$0
@@ -22,8 +22,13 @@ FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
 
 echo "$FILES_TO_DELETE"
 
+echo "script started executing at $Date"
+
+
 while read line # read command reads a line from the input and assigns it to the variable line. 
 do
     echo "Deleting $line" &>> $LOGFILE
     rm -rf $line
 done <<< $FILES_TO_DELETE    
+
+# you can run your scripts in two ways 1. sh script_name --> now need to have execute permission 2. ./script_name --> should have execute permission
